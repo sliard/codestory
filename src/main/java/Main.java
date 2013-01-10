572cb63a88a9d8861c7f9f7dc339a466fb4d3b00
Nicolas De Loof <nicolas.deloof@gmail.com>
2013-01-10 09:53:59 +0100

    some refactoring to avoid code duplication

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,15 +6,20 @@ import java.io.IOException;
 
 public class Main extends HttpServlet {
 
+    private static final String QUELLE_EST_TON_ADRESSE_EMAIL = "Quelle est ton adresse email";
+    private static final String ES_TU_ABONNE_A_LA_MAILING_LIST = "Es tu abonne a la mailing list(OUI/NON)";
+
     @Override
     protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         String q = req.getParameter("q");
-        if (q.equals("Quelle est ton adresse email")) {
-            resp.getWriter().print("nicolas.deloof@gmail.com");
-        } else if (q.equals("Es tu abonne a la mailing list(OUI/NON)")) {
-            resp.getWriter().print("OUI");
+        String r;
+        if (q.equals(QUELLE_EST_TON_ADRESSE_EMAIL)) {
+            r = "nicolas.deloof@gmail.com";
+        } else if (q.equals(ES_TU_ABONNE_A_LA_MAILING_LIST)) {
+            r = "OUI";
         } else {
-            resp.getWriter().print("@see http://code-story.net");
+            r = "@see http://code-story.net";
         }
+        resp.getWriter().print(r);
    }
 }
